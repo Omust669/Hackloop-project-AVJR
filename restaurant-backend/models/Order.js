653f033e1037e834aef15ed9e878eb951e15233e
ADARSH-HN <adarshnaik7338@gmail.com>
2024-11-21 06:29:39 +0530

    added agitignore file

--- restaurant-backend/models/Order.js
+++ restaurant-backend/models/Order.js
@@ -4,17 +4,23 @@ const mongoose = require('mongoose');
 const OrderSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
+    phone: { type: String, required: true },
     arrivalTime: { type: Date, required: true },
     items: [
         {
-            name: String,
-            quantity: Number,
-            price: Number
+            name: { type: String, required: true },
+            quantity: { type: Number, required: true, min: 1 },
+            price: { type: Number, required: true, min: 0 }
         }
     ],
     totalPrice: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now }
 });
+
+// Add a custom validator to prevent empty carts
+OrderSchema.path('items').validate(function (items) {
+    return items.length > 0; // Ensure that there is at least one item in the array
+}, 'A cart cannot be empty');
 
 // Export the model
 module.exports = mongoose.model('Order', OrderSchema);
